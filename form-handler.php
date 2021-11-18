27561e694b25366211d86af734a3d791f51fa764
ashishgupta-111 <you@example.com>
2021-11-18 12:11:04 +0530

    Ashi

---
+++ form-handler.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'info@yourwebsite.com';
+$email_subject = 'NEw form Submission';
+$email_body = "User name : $name.\n".
+               "User Email : $visiotr_email.\n".
+               "Subject : $subject.\n".
+               "User Message: $message .\n".
+$to = 'ashuguptaji59@gmail.com';
+$header = "from: $email_form \r\n";
+$header .= "from: $visitor_form \r\n";
+
+mail($to,$email_subject,$email_body,$header);
+
+header("Location : contact.html");
+
+
+
+
+?>
